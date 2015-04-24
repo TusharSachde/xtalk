824c39d738426badd1ee1f43e4df545b56b38829
Chintan Shah <chintan@wohlig.com>
2015-04-24 12:45:56 +0530

    change for call

--- js/controllers.js
+++ js/controllers.js
@@ -12,28 +12,40 @@ angular.module('starter.controllers', [])
         console.log('Dialer Hidden');
     };
     $scope.call = function(number) {
-        document.location.href = "tel:" +number;
+        phonedialer.dial(
+            number,
+            function(err) {
+                if (err == "empty") console.log("Unknown phone number");
+                else console.log("Dialer Error:" + err);
+            },
+            function(success) {
+                console.log('Dialing succeeded');
+            }
+        );
+        //document.location.href = "tel:" + number;
         console.log('Calling');
     };
     $scope.sms = function(number) {
-        document.location.href = "sms:" +number;
+        document.location.href = "sms:" + number;
         console.log('SMS');
     };
     $scope.mail = function(email) {
-        document.location.href = "mailto:" +email;
+        document.location.href = "mailto:" + email;
         console.log('Mail');
     };
-    $scope.phone={};
-    $scope.phone.number="";
-    
-    $scope.phonenum=function(number) {
-        console.log("number presses "+number);
-        $scope.phone.number+=""+number;
+    $scope.phone = {};
+    $scope.phone.number = "";
+
+    $scope.phonenum = function(number) {
+        console.log("number presses " + number);
+        $scope.phone.number += "" + number;
     };
-    $scope.phoneback=function() {
-        $scope.phone.number=$scope.phone.number.slice(0, -1);
+    $scope.phoneback = function() {
+        $scope.phone.number = $scope.phone.number.slice(0, -1);
     };
-    
+
+
+
 })
 
 .controller('NewsCtrl', function($scope) {
