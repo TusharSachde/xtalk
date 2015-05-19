ff5a1f328596a4ca3d6809fce2cf340c6cd9140e
Tushar <tushar@wohlig.com>
2015-05-19 11:17:45 +0530

    Disabled touch swipe & pop up working without sms plugin error

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function ($scope, $ionicPopup, $location, MyServices) {
-    var readsmsCallback = function (otp) {
+.controller('AppCtrl', function($scope, $ionicPopup, $location, MyServices) {
+    var readsmsCallback = function(otp) {
         if (!otp) {
             conole.log("No Otp");
         } else {
@@ -13,18 +13,35 @@ angular.module('starter.controllers', [])
     MyServices.readsms(readsmsCallback);
 })
 
-.controller('EnterCtrl', function ($scope, $ionicSlideBoxDelegate, $ionicPopup, MyServices) {
+.controller('EnterCtrl', function($scope, $ionicSlideBoxDelegate, $ionicPopup, MyServices) {
 
     $scope.personal = {};
-//    $scope.next = function () {
-//        
-//    };
+    //    $scope.next = function () {
+    //        
+    //    };
 
-    var registerSuccess = function (data, status) {
+    //Popup for dint get OTP
+    $scope.showAlert = function() {
+        console.log('Dint get OTP?');
+        var alertPopup = $ionicPopup.alert({
+            title: "Didn't get the OTP ?",
+            template: 'Please try resending the OTP.',
+            buttons: [{
+                text: 'Resend',
+                type: 'button-positive button-outline'
+            }],
+        });
+        alertPopup.then(function(res) {
+            console.log('OTP Resent !');
+        })
+    };
+
+
+    var registerSuccess = function(data, status) {
         console.log("Registered " + data);
         userid = parseInt(data);
     };
-    $scope.phonesubmit = function (phoneno) {
+    $scope.phonesubmit = function(phoneno) {
         if (phoneno.phone.isNumber) {
             personalcontact = phoneno.phone;
             MyServices.register(phoneno.phone).success(registerSuccess);
@@ -39,27 +56,17 @@ angular.module('starter.controllers', [])
     //    };
     //    MyServices.query("SELECT * FROM LOGS", logload);
 
-    $scope.previous = function () {
+    $scope.disableSwipe = function() {
+        $ionicSlideBoxDelegate.enableSlide(false);
+    };
+
+    $scope.previous = function() {
         $ionicSlideBoxDelegate.previous();
     };
 
     // Called each time the slide changes
-    $scope.slideChanged = function (index) {
+    $scope.slideChanged = function(index) {
         $scope.slideIndex = index;
-    };
-
-    $scope.showAlert = function () {
-        var alertPopup = $ionicPopup.alert({
-            title: "Didn't get the OTP ?",
-            template: 'Please try resending the OTP.',
-            buttons: [{
-                text: 'Resend',
-                type: 'button-positive button-outline'
-            }],
-        });
-        alertPopup.then(function (res) {
-            console.log('OTP Resent !');
-        })
     };
 
     //    var readsmsCallback = function (otp) {
@@ -72,11 +79,11 @@ angular.module('starter.controllers', [])
     //    MyServices.readsms(readsmsCallback);
 })
 
-.controller('ProfileCtrl', function ($scope, $location, MyServices) {
+.controller('ProfileCtrl', function($scope, $location, MyServices) {
 
     //Contacts Sending 
     var myconarr = [];
-    var contactCallback = function (contact) {
+    var contactCallback = function(contact) {
         console.log("contacts");
         console.log(contact);
         if (contact) {
@@ -120,7 +127,7 @@ angular.module('starter.controllers', [])
         //            MyServices.query('INSERT INTO MYCONTACTS (user,name,email,contactno) VALUES (?, ?, ?, ?)', [myconarr[i].user, myconarr[i].name, myconarr[i].email, myconarr[i].contactno], insertsuccess);
         //        }
 
-        var sendContactsSuccess = function (data, success) {
+        var sendContactsSuccess = function(data, success) {
             console.log("Contact already Registered" + data);
             contact = data;
             console.log(contact);
@@ -131,12 +138,12 @@ angular.module('starter.controllers', [])
 
     $scope.mergecard = {};
     $scope.personal = {};
-    $scope.CardDetails = function (card) {
+    $scope.CardDetails = function(card) {
         mycard1 = card;
         //        console.log($scope.mycard);
         $location.path("/profile/personal");
     };
-    $scope.PersonalDetails = function (card) {
+    $scope.PersonalDetails = function(card) {
         $scope.mycard2 = card;
         console.log(mycard1);
         console.log($scope.mycard2);
@@ -145,7 +152,7 @@ angular.module('starter.controllers', [])
         console.log("heycgyi" + personalcontact);
         console.log($scope.mergecard);
 
-        var createCardSucess = function (data, status) {
+        var createCardSucess = function(data, status) {
             console.log("HEy" + data)
         }
         MyServices.createCard($scope.mergecard).success(createCardSucess);
@@ -155,17 +162,17 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('Circle1Ctrl', function ($scope) {})
+.controller('Circle1Ctrl', function($scope) {})
 
-.controller('Circle2Ctrl', function ($scope) {})
+.controller('Circle2Ctrl', function($scope) {})
 
-.controller('Circle3Ctrl', function ($scope) {})
+.controller('Circle3Ctrl', function($scope) {})
 
-.controller('TabCtrl', function ($scope, $location) {
+.controller('TabCtrl', function($scope, $location) {
 
 })
 
-.controller('ProfileShareCtrl', function ($scope, MyServices) {
+.controller('ProfileShareCtrl', function($scope, MyServices) {
     //
     //    $scope.mycon = [{
     //        address: "sfjk",
@@ -252,28 +259,28 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('ProfileGetCtrl', function ($scope, MyServices) {
+.controller('ProfileGetCtrl', function($scope, MyServices) {
     $scope.contacts = MyServices.all();
     console.log($scope.contacts);
     $scope.$apply();
 })
 
-.controller('DashCtrl', function ($scope) {
+.controller('DashCtrl', function($scope) {
 
 })
 
-.controller('ChatsCtrl', function ($scope) {})
+.controller('ChatsCtrl', function($scope) {})
 
-.controller('SpingbookCtrl', function ($scope, MyServices, $ionicPopover, $ionicModal, $location) {
+.controller('SpingbookCtrl', function($scope, MyServices, $ionicPopover, $ionicModal, $location) {
 
     $scope.search = false;
     $scope.filterbtn = false;
-    $scope.showsearch = function () {
+    $scope.showsearch = function() {
         console.log('Search Clicked');
         $scope.search = !$scope.search;
     };
 
-    $scope.filtertoggle = function (keyEvent) {
+    $scope.filtertoggle = function(keyEvent) {
         if (keyEvent.which === 13) {
             console.log('Filter Enter Clicked');
             $scope.filterbtn = true;
@@ -284,63 +291,63 @@ angular.module('starter.controllers', [])
 
     $scope.contacts = MyServices.all();
     $scope.showdailer = false;
-    $scope.hidedialer = function () {
+    $scope.hidedialer = function() {
         $scope.showdailer = false;
         console.log('Dialer Hidden');
     };
-    $scope.call = function (number) {
+    $scope.call = function(number) {
         phonedialer.dial(
             number,
-            function (err) {
+            function(err) {
                 if (err == "empty") console.log("Unknown phone number");
                 else console.log("Dialer Error:" + err);
             },
-            function (success) {
+            function(success) {
                 console.log('Dialing succeeded');
             }
         );
         //document.location.href = "tel:" + number;
         console.log('Calling');
     };
-    $scope.sms = function (number) {
+    $scope.sms = function(number) {
         document.location.href = "sms:" + number;
         console.log('SMS');
     };
-    $scope.mail = function (email) {
+    $scope.mail = function(email) {
         document.location.href = "mailto:" + email;
         console.log('Mail');
     };
     $scope.phone = {};
     $scope.phone.number = "";
 
-    $scope.phonenum = function (number) {
+    $scope.phonenum = function(number) {
         console.log("number presses " + number);
         $scope.phone.number += "" + number;
     };
-    $scope.phoneback = function () {
+    $scope.phoneback = function() {
         $scope.phone.number = $scope.phone.number.slice(0, -1);
     };
 
-    $scope.phonedelete = function () {
+    $scope.phonedelete = function() {
         $scope.phone.number = "";
     };
 
 
     $ionicPopover.fromTemplateUrl('templates/popover.html', {
         scope: $scope
-    }).then(function (popover) {
+    }).then(function(popover) {
         $scope.popover = popover;
     });
 
-    $scope.openPopover = function ($event) {
+    $scope.openPopover = function($event) {
         $scope.popover.show($event);
     };
-    $scope.closePopover = function () {
+    $scope.closePopover = function() {
         $scope.popover.hide();
     };
 
     //Cleanup the popover when we're done with it!
-    $scope.$on('$destroy', function () {
+    $scope.$on('$destroy', function() {
         $scope.popover.remove();
     });
 
@@ -350,14 +357,14 @@ angular.module('starter.controllers', [])
         id: '1',
         scope: $scope,
         animation: 'slide-in-up'
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.oModal1 = modal;
     });
 
-    $scope.openfilter = function () {
+    $scope.openfilter = function() {
         $scope.oModal1.show();
     }
-    $scope.closefilter = function () {
+    $scope.closefilter = function() {
         $scope.oModal1.hide();
     };
 
@@ -366,35 +373,35 @@ angular.module('starter.controllers', [])
         id: '2',
         scope: $scope,
         animation: 'slide-in-up'
-    }).then(function (modal) {
+    }).then(function(modal) {
         $scope.oModal2 = modal;
     });
 
-    $scope.openadvance = function () {
+    $scope.openadvance = function() {
         $scope.oModal2.show();
     }
-    $scope.closeadvance = function () {
+    $scope.closeadvance = function() {
         $scope.oModal2.hide();
     };
 
 
-    $scope.searchpage = function () {
+    $scope.searchpage = function() {
         $location.url('/circle/circle1');
         console.log('searchpage');
     }
 
-    $scope.spingpage = function () {
+    $scope.spingpage = function() {
         $location.url('/tab/spingbook');
         console.log('spingpage');
     }
 
 })
 
-.controller('InSpingbookCtrl', function ($scope, MyServices, $stateParams) {
+.controller('InSpingbookCtrl', function($scope, MyServices, $stateParams) {
     $scope.contact = MyServices.get($stateParams.Id);
 })
 
-.controller('NewsCtrl', function ($scope) {
+.controller('NewsCtrl', function($scope) {
     $scope.settings = {
         enableNews: true
     };
